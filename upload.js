9a0fb86ab0c1ae533a1eaba980b9f247f1a3524c
zddhandsome <71329412+zddhandsome@users.noreply.github.com>
2021-11-06 12:23:19 +0800

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,11 +1,3 @@
-var openFile = function(event) {
-    var input = event.target;
-    var reader = new FileReader();
-    reader.onload = function() {
-        if(reader.result) {
-            //显示文件内容
-            $("#output").html(reader.result);
-        }
-    };
-    reader.readAsText(input.files[0]);
-};
+$(function(){
+    $("#test3").load("https://github.com/zddhandsome/webserver-ex09/blob/main/upload.js");
+})
